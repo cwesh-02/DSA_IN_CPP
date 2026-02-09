96a27a7828e7853c7e26f773dd57e6babd32c5b6
Shivesh_Kumar <gamingshivesh1122@gmail.com>
2026-02-09 16:24:31 +0530

    Started Implementing AVL

---
+++ Trees/AvlTree/BasicsImplementation/Avl.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+public:
+int data , height;
+Node* left , *right;
+
+    Node(int val)
+    {
+        data = val;
+        height = 1;
+        left = right = NULL;
+    }
+};
+
+int GetHeight(Node* root)
+{
+    if(!root) return NULL;
+    return root->height;
+}
+    
+Node* RotateRight(Node* root)
+{
+    Node* temp = root->left;
+    root->left = temp->right;
+    temp->right = root;
+    return temp;
+}
+
+Node* RotateLeft(Node* root)
+{
+    Node* temp = root->right;
+    root->right = temp->left;
+    temp->left = root;
+    return temp;
+}
+
+Node* Balance(Node* root)
+{
+    if(!root)
+    {
+        return NULL;
+    }
+
+    
+}
+
+Node* InsertBST(Node* root , int value)
+{
+    if(!root)
+    {
+        return new Node(value);
+    }
+
+    if(value<root->data)
+    {
+        root->left = InsertBST(root->left , value);
+    }
+    else
+    root->right = InsertBST(root->right , value);
+
+    return Balance(root);
+}
+
+int main()
+{
+    Node* root = NULL;
+    int value;
+    while(1)
+    {
+        if(value!=-1)
+        {
+            root=InsertBST(root,value);
+        }
+        else
+        break;
+    }
+    return 0;
+}
