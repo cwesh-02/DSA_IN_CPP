b4b3e9befcb1126cda0b7cb1ad398e696b9b260c
Shivesh_Kumar <gamingshivesh1122@gmail.com>
2026-02-11 23:20:37 +0530

    updated

---
+++ Array/Set/Longest_Balanced_Subarray_I.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) 
+    {
+        int ans =0;
+        int n=nums.size();
+        for(int i =0;i<n;i++)
+        {
+            unordered_set<int> even;
+            unordered_set<int> odd;
+
+            for(int j=i;j<n;j++)
+            {
+                if(nums[j]%2==0)
+                {
+                    even.insert(nums[j]);
+                }
+
+                else
+                {
+                    odd.insert(nums[j]);
+                }
+
+                if(even.size()==odd.size())
+                {
+                    ans=max(j-i+1 , ans);
+                }
+
+            }
+        }
+
+        return ans;
+    }
+};
