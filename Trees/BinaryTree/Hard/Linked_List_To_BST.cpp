36d1ddafe8bb20dbfe18b9efa0d6925c6f61474e
Shivesh_Kumar <gamingshivesh1122@gmail.com>
2026-02-19 11:44:12 +0530

    updated

---
+++ Trees/BinaryTree/Hard/Linked_List_To_BST.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* solve(ListNode* head)
+    {
+        if(!head) return NULL;
+        
+        ListNode* slow=head;
+        ListNode* fast=head;
+        ListNode* prev=NULL;
+        
+        while(fast && fast->next)
+        {
+            prev = slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        TreeNode* root = new TreeNode(slow->val);
+        if (!head->next) return root;
+        prev->next=NULL;
+
+        root->left = solve(head);
+        root->right = solve(slow->next);
+
+        return root;
+    }
+
+    TreeNode* sortedListToBST(ListNode* head) 
+    {
+        return solve(head);
+    }
+};
