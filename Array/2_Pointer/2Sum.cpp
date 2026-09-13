7452987efe706e37bd33e87eeb7e2e0fe85a5ebb
cwesh-02 <gamingshivesh1122@gmail.com>
2026-09-13 21:42:41 +0530

    kya din agye h 2sum nhi horha ek baarme

---
+++ Array/2_Pointer/2Sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) 
+    {
+        
+        int n = arr.size();
+        sort(arr.begin() , arr.end());
+        
+        int i  = 0;
+        int j = n-1;
+        
+        while(i<j)
+        {
+            if(arr[i] + arr[j] == target) return true;
+            
+            else if(arr[i] + arr[j] > target)
+            j--;
+            
+            else
+            i++;
+        }
+        
+        return false;
+        
+    }
+};
